6fa087c96a98bc0cd5faee003059adcfc0946610
Pavel <guzenko.pavel@gmail.com>
2013-09-15 23:44:41 +0700

    Vse ia sdelal svoe

--- Widget/src/com/example/widget/MainActivity.java
+++ Widget/src/com/example/widget/MainActivity.java
@@ -19,30 +19,26 @@ public class MainActivity extends Activity {
 	final String DIR_SD = "MyFiles";
 	final String FILENAME_SD_1 = "fileSD1_new";
 	final String FILENAME_SD_2 = "fileSD2_new";
+
 	ArrayList<String> full_text;
 	ArrayList<String> full_text2;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		ProgressDialog mProgressDialog;
 
 		// instantiate it within the onCreate method
-		mProgressDialog = new ProgressDialog(this);
-		mProgressDialog.setMessage("A message");
-		mProgressDialog.setIndeterminate(true);
-		mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-		mProgressDialog.setCancelable(true);
-		full_text=new ArrayList<String>();
-		full_text2=new ArrayList<String>();
+
+	
+		full_text = new ArrayList<String>();
+		full_text2 = new ArrayList<String>();
 		DownloadTask downloadTask = new DownloadTask(this);
 		downloadTask.execute("http://rp5.ru/xml/6036/ru");
+
 		readFile();
-		full_text2.add(full_text.get(6));
 		peredelivaem();
-		//TextView txt2=(TextView) findViewById(R.id.textView1);
-		//txt2.setText(full_text2.get(12));
-		
+
 	}
 
 	@Override
@@ -52,22 +48,20 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	
+	// читаем в full_text из файла
 	void readFile() {
 
-	
-	
 		File sdPath = Environment.getExternalStorageDirectory();
 		sdPath = new File(sdPath.getAbsolutePath() + "/" + DIR_SD);
 		File sdFile = new File(sdPath, FILENAME_SD_1);
-		//TextView txt2=(TextView) findViewById(R.id.textView1);
+		// TextView txt2=(TextView) findViewById(R.id.textView1);
 
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(sdFile));
 			String str = new String();
-			//txt2.setText("");
+			// txt2.setText("");
 			while ((str = br.readLine()) != null) {
-			//	txt2.setText(txt2.getText().toString() + str + '\n');
+				// txt2.setText(txt2.getText().toString() + str + '\n');
 				full_text.add(str);
 			}
 			br.close();
@@ -79,12 +73,13 @@ public class MainActivity extends Activity {
 			e.printStackTrace();
 		}
 
-
 	}
-	void peredelivaem()
-	{
+
+	// сдесь мы вибираем из full_text нужные нам тэги в full_text2, а потом
+	// возавращаем нужные в full_text
+	void peredelivaem() {
 		full_text2.add(full_text.get(6));
-		
+
 		full_text2.add(full_text.get(15));
 		full_text2.add(full_text.get(18));
 		full_text2.add(full_text.get(19));
@@ -95,7 +90,7 @@ public class MainActivity extends Activity {
 		full_text2.add(full_text.get(24));
 		full_text2.add(full_text.get(25));
 		full_text2.add(full_text.get(26));
-		
+
 		full_text2.add(full_text.get(30));
 		full_text2.add(full_text.get(33));
 		full_text2.add(full_text.get(34));
@@ -106,7 +101,7 @@ public class MainActivity extends Activity {
 		full_text2.add(full_text.get(39));
 		full_text2.add(full_text.get(40));
 		full_text2.add(full_text.get(41));
-		
+
 		full_text2.add(full_text.get(45));
 		full_text2.add(full_text.get(48));
 		full_text2.add(full_text.get(49));
@@ -117,7 +112,7 @@ public class MainActivity extends Activity {
 		full_text2.add(full_text.get(54));
 		full_text2.add(full_text.get(55));
 		full_text2.add(full_text.get(56));
-		
+
 		full_text2.add(full_text.get(60));
 		full_text2.add(full_text.get(63));
 		full_text2.add(full_text.get(64));
@@ -128,7 +123,26 @@ public class MainActivity extends Activity {
 		full_text2.add(full_text.get(69));
 		full_text2.add(full_text.get(70));
 		full_text2.add(full_text.get(71));
+		full_text.clear();
+		for (int i = 0; i < full_text2.size(); i++) {
+			full_text.add(reboot(full_text2.get(i)));
+
+		}
 	}
 
-	
+	// сдесь убираются тэги типа <> </>
+	String reboot(String s) {
+		String k = "";
+		char ss[] = s.toCharArray();
+
+		for (int i = 1 + s.indexOf('>'); i < ss.length; i++) {
+			if (ss[i] == '<')
+				break;
+			{
+				k = k + ss[i];
+			}
+		}
+		return k;
+	}
+
 }

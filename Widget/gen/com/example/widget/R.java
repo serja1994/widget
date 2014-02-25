42484cf160cd560fd6f1e9b97bec6828ea9cebd9
guzenkopavel <guzenko.pavel@gmail.com>
2014-02-25 21:51:07 +0700

    asd

--- Widget/gen/com/example/widget/R.java
+++ Widget/gen/com/example/widget/R.java
@@ -24,14 +24,27 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int textView1=0x7f080000;
-        public static final int tv=0x7f080001;
+        public static final int action_settings=0x7f08000e;
+        public static final int activity2_listView1=0x7f080000;
+        public static final int button1=0x7f080001;
+        public static final int list2_textView1=0x7f080007;
+        public static final int list2_textView2=0x7f080008;
+        public static final int list2_textView3=0x7f080009;
+        public static final int list2_textView4=0x7f08000a;
+        public static final int list2_textView5=0x7f08000b;
+        public static final int list2_textView6=0x7f08000c;
+        public static final int list2_textView7=0x7f08000d;
+        public static final int listView1=0x7f080003;
+        public static final int list_textView1=0x7f080004;
+        public static final int list_textView2=0x7f080005;
+        public static final int list_textView3=0x7f080006;
+        public static final int textView1=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int widget=0x7f030001;
-        public static final int widget_metadata=0x7f030002;
+        public static final int activity2=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int list=0x7f030002;
+        public static final int list2=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -39,7 +52,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int parser_den=0x7f050003;
+        public static final int parser_noch=0x7f050002;
+        public static final int parser_utro=0x7f050005;
+        public static final int parser_vecher=0x7f050004;
     }
     public static final class style {
         /** 
--- Widget/src/com/example/widget/MainActivity.java
+++ Widget/src/com/example/widget/MainActivity.java
@@ -1,148 +1,236 @@
 package com.example.widget;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
+import android.os.PowerManager;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	final String DIR_SD = "MyFiles";
-	final String FILENAME_SD_1 = "fileSD1_new";
-	final String FILENAME_SD_2 = "fileSD2_new";
-
-	ArrayList<String> full_text;
-	ArrayList<String> full_text2;
+	final String CITY = "http://informer.gismeteo.ru/xml/34601_1.xml";
+	DownloadTask downloadTask;
+	ListView lvMain;
+	Button b;
+	Parser ps;
+	ArrayList<DataToList> dataToList;
+	ArrayList<String> dataFromGismeteo;
+	TextView txt;
+	Adapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		lvMain = (ListView) findViewById(R.id.listView1);
+		downloadTask = new DownloadTask(this);
+		txt = (TextView) findViewById(R.id.textView1);
+		b = (Button) findViewById(R.id.button1);
+		ps = new Parser();
+		dataToList = new ArrayList<DataToList>();
 
-		// instantiate it within the onCreate method
+		b.setOnClickListener(new OnClickListener() {
 
-	
-		full_text = new ArrayList<String>();
-		full_text2 = new ArrayList<String>();
-		DownloadTask downloadTask = new DownloadTask(this);
-		downloadTask.execute("http://rp5.ru/xml/6036/ru");
+			@Override
+			public void onClick(View arg0) {
+				if (isOnline(arg0.getContext())) {
 
-		readFile();
-		peredelivaem();
-
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	// читаем в full_text из файла
-	void readFile() {
-
-		File sdPath = Environment.getExternalStorageDirectory();
-		sdPath = new File(sdPath.getAbsolutePath() + "/" + DIR_SD);
-		File sdFile = new File(sdPath, FILENAME_SD_1);
-		// TextView txt2=(TextView) findViewById(R.id.textView1);
-
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(sdFile));
-			String str = new String();
-			// txt2.setText("");
-			while ((str = br.readLine()) != null) {
-				// txt2.setText(txt2.getText().toString() + str + '\n');
-				full_text.add(str);
+					downloadTask = (DownloadTask) new DownloadTask(arg0
+							.getContext()).execute(CITY);
+				} else {
+					Toast.makeText(arg0.getContext(),
+							"Отсутствует соединение с интернетом",
+							Toast.LENGTH_LONG).show();
+				}
 			}
-			br.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-		}
+		});
+		lvMain.setOnItemClickListener(new OnItemClickListener() {
 
-	}
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				try {
+					dataFromGismeteo = downloadTask.get();
 
-	// сдесь мы вибираем из full_text нужные нам тэги в full_text2, а потом
-	// возавращаем нужные в full_text
-	void peredelivaem() {
-		full_text2.add(full_text.get(6));
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} catch (NullPointerException e) {
+					txt.setText("Нет данных");
+					e.printStackTrace();
 
-		full_text2.add(full_text.get(15));
-		full_text2.add(full_text.get(18));
-		full_text2.add(full_text.get(19));
-		full_text2.add(full_text.get(20));
-		full_text2.add(full_text.get(21));
-		full_text2.add(full_text.get(22));
-		full_text2.add(full_text.get(23));
-		full_text2.add(full_text.get(24));
-		full_text2.add(full_text.get(25));
-		full_text2.add(full_text.get(26));
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+				Intent intent = new Intent(arg0.getContext(), Activity2.class);
+				intent.putExtra("str", dataFromGismeteo);
+				intent.putExtra("position", arg2);
+				startActivity(intent);
 
-		full_text2.add(full_text.get(30));
-		full_text2.add(full_text.get(33));
-		full_text2.add(full_text.get(34));
-		full_text2.add(full_text.get(35));
-		full_text2.add(full_text.get(36));
-		full_text2.add(full_text.get(37));
-		full_text2.add(full_text.get(38));
-		full_text2.add(full_text.get(39));
-		full_text2.add(full_text.get(40));
-		full_text2.add(full_text.get(41));
-
-		full_text2.add(full_text.get(45));
-		full_text2.add(full_text.get(48));
-		full_text2.add(full_text.get(49));
-		full_text2.add(full_text.get(50));
-		full_text2.add(full_text.get(51));
-		full_text2.add(full_text.get(52));
-		full_text2.add(full_text.get(53));
-		full_text2.add(full_text.get(54));
-		full_text2.add(full_text.get(55));
-		full_text2.add(full_text.get(56));
-
-		full_text2.add(full_text.get(60));
-		full_text2.add(full_text.get(63));
-		full_text2.add(full_text.get(64));
-		full_text2.add(full_text.get(65));
-		full_text2.add(full_text.get(66));
-		full_text2.add(full_text.get(67));
-		full_text2.add(full_text.get(68));
-		full_text2.add(full_text.get(69));
-		full_text2.add(full_text.get(70));
-		full_text2.add(full_text.get(71));
-		full_text.clear();
-		for (int i = 0; i < full_text2.size(); i++) {
-			full_text.add(reboot(full_text2.get(i)));
-
-		}
-	}
-
-	// сдесь убираются тэги типа <> </>
-	String reboot(String s) {
-		String k = "";
-		char ss[] = s.toCharArray();
-
-		for (int i = 1 + s.indexOf('>'); i < ss.length; i++) {
-			if (ss[i] == '<')
-				break;
-			{
-				k = k + ss[i];
 			}
-		}
-		return k;
+		});
+
 	}
 
+	class DownloadTask extends AsyncTask<String, Integer, ArrayList<String>> {
+		ArrayList<String> str = new ArrayList<String>();
+		private Context context;
+		ProgressDialog mProgressDialog;
+
+		public DownloadTask(Context context) {
+			this.context = context;
+		}
+
+		@Override
+		protected ArrayList<String> doInBackground(String... sUrl) {
+
+			InputStream input = null;
+			HttpURLConnection connection = null;
+			try {
+				URL url = new URL(sUrl[0]);
+				connection = (HttpURLConnection) url.openConnection();
+				connection.connect();
+
+				if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+
+					return null;
+				}
+
+				input = connection.getInputStream();
+				BufferedReader r = new BufferedReader(new InputStreamReader(
+						input));
+
+				String line;
+				while ((line = r.readLine()) != null) {
+					str.add(line);
+
+				}
+
+			} catch (Exception e) {
+				return null;
+			} finally {
+				if (connection != null)
+					connection.disconnect();
+			}
+
+			return str;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... progress) {
+			super.onProgressUpdate(progress);
+
+		}
+
+		protected void onPostExecute(ArrayList<String> result) {
+			dataToList.clear();
+			txt.setText("Данные взяты с сайта gismeteo: " + CITY);
+			try {
+				ps.parserLow(result, dataToList);
+			} catch (NullPointerException e) {
+				Toast.makeText(context, "Не удалось подключиться к серверу",
+						Toast.LENGTH_SHORT).show();
+				txt.setText("Нет данных");
+				e.printStackTrace();
+			}
+			adapter = new Adapter(context, dataToList, 0);
+			lvMain.setAdapter(adapter);
+
+		}
+	}
+
+	public class Adapter extends BaseAdapter {
+		Context ctx;
+		int positionz, kol;
+		String proba = null;
+		LayoutInflater lInflater;
+		ArrayList<DataToList> objects;
+
+		Adapter(Context context, ArrayList<DataToList> products, int pos) {
+			ctx = context;
+			positionz = pos;
+			objects = products;
+			lInflater = (LayoutInflater) ctx
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		}
+
+		@Override
+		public int getCount() {
+			return objects.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return objects.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view = convertView;
+			if (view == null) {
+				view = lInflater.inflate(R.layout.list, lvMain, false);
+			}
+
+			DataToList p = getProduct(position);
+			((TextView) view.findViewById(R.id.list_textView1))
+					.setText(p.first);
+			((TextView) view.findViewById(R.id.list_textView2))
+					.setText(p.second);
+
+			((TextView) view.findViewById(R.id.list_textView3))
+					.setText(p.third);
+
+			return view;
+		}
+
+		DataToList getProduct(int position) {
+			return ((DataToList) getItem(position));
+		}
+	}
+
+	public static boolean isOnline(Context context) {
+		ConnectivityManager cm = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+			return true;
+		}
+		return false;
+	}
 }
